6ddb36445f97abb679c063cf7ebc0d34e6ae68f9
MrEBONG <thierryngabe@icloud.com>
2024-06-03 17:01:16 +0100

    fixed errors

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import styles from './page.module.css'
-import Image from 'next/image';
-import Link from 'next/link';
-import { revalidateTag } from 'next/cache';
+"use client"
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { revalidateTag } from "next/cache";
 
-   
-  
- async function getData() {
-  const res = await fetch("/api/posts", { next: {tags: ['collection']}})
-  
-  revalidateTag('collection')
+const page = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("/api/posts");
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
-const page = async () => {
+      const data = await res.json();
+      setData(data);
 
-    const data = await getData();
-   
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+    ;
+    };
+
+    getData();
+  }, []);
+
   return (
     <>
-
-     <div className={styles.backgroundImage}>
-     </div>
-     <div className={styles.hope}>
-     <p className={styles.desc}>
-        Join me for a journey
-          Through Cameroon <br />
-           vibrant cities.
-      </p>
-     </div>
-     <div className={styles.container}>
-        {data.map(item => (
-       <div className={styles.card} key={item.id}>
+      <div className={styles.backgroundImage}></div>
+      <div className={styles.hope}>
+        <p className={styles.desc}>
+          Join me for a journey Through Cameroon <br />
+          vibrant cities.
+        </p>
+      </div>
+      <div className={styles.container}>
+        {data.map((item) => (
+          <div className={styles.card} key={item.id}>
             <Link href={`/${item.id}`}>
               <Image
-              className={styles.image}
+                className={styles.image}
                 src={item.img}
                 alt={item.title}
                 width={400}
@@ -47,12 +47,9 @@ const page = async () => {
             <h1>{item.title}</h1>
             <p className={styles.write}>{item.description}</p>
           </div>
-         ))}
-     </div>
-  
- 
-     </>
-  
+        ))}
+      </div>
+    </>
   );
-}
+};
 export default page;
