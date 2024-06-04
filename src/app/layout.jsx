69ab47cea1d12da34fdf1ef03d0a30cac8b15289
MrEBONG <thierryngabe@icloud.com>
2024-06-04 11:01:27 +0100

    clean

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
             <Footer />
           </AuthProvider>
         </ThemeProvider>
-        <script src="//code.tidio.co/btk9udn3buasyxq0hrzuqpljc4ddlfhh.js" async></script>
+        <script src="//code.tidio.co/2ptymkyljlraxn5oip8wqz1pieww3ff7.js" async></script>
       </body>
     </html>
   );
